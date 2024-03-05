14832c9068710b703a8aa2ac34ccb8389ea1fb54
jiepengtan <jiepengtan@gmail.com>
2024-03-05 10:30:35 +0800

    Auto commit: Tue Mar  5 10:30:34 AM CST 2024

--- scripts/train_mesh.sh
+++ scripts/train_mesh.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
+#!/bin/bash
 pip install pyyaml
 pip install numpy==1.23.0
 
-dir_name=ft_wp
+dir_name=ft_mesh
 if [ $# -ge 1 ]; then
     dir_name=$1
 fi
 
 dir_path=./checkpoint/mesh/$dir_name
+rm -rf $dir_path
 mkdir -p $dir_path
 
-echo "train in $dir_path "
-rm nohup.out
-ps -ef | grep train_mesh | awk '{print $2}' | xargs kill -9
+ps -ef | grep python | awk '{print $2}' | xargs kill -9
+
+nohup_train_log=$dir_path/nohup_train.out
+
+if [ -f $nohup_train_log ]; then
+    echo "The file $nohup_train_log exist."
+    rm -rf $nohup_train_log
+fi
 
 # start train task
-nohup python train_mesh.py --config configs/mesh/MB_ft_pw3d.yaml --pretrained checkpoint/pretrain/MB_release --checkpoint checkpoint/mesh/${dir_name} &
+echo "==============start train $dir_path =================="
+
+nohup python train_mesh.py --config configs/mesh/MB_ft_h36m.yaml --pretrained checkpoint/pretrain/MB_release --checkpoint checkpoint/mesh/${dir_name} > ${nohup_train_log}  2>&1 &
 
 
-echo "start tensorboard "
+echo "==============start tensorboard =================="
 # start tensorboard
-cd ./checkpoint/mesh
-rm nohup.out
+nohup_tf_log=$dir_path/tf_nohup_.out
 ps -ef | grep tensorboard | awk '{print $2}' | xargs kill -9
-nohup tensorboard --port 6007 --logdir ./${dir_name}/logs/ &
-cd ../../
+nohup tensorboard --port 6007 --logdir $dir_path/logs/ > ${nohup_tf_log}  2>&1 &
 
 # view the train logs
-tail -f nohup.out
-
+tail -f $nohup_train_log
