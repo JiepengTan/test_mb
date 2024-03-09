c0678ea5c39c9b528cd291bb78b4216375f229f2
jiepengtan <jiepengtan@gmail.com>
2024-03-09 09:39:50 +0800

    Auto commit: Sat Mar  9 09:39:50     2024

--- tools/render_infer_result.py
+++ tools/render_infer_result.py
@@ -8,7 +8,8 @@ sys.path.insert(0, os.getcwd())
 from lib.utils.vismo import render_and_save
 
 fps_in = 30
-max_epoch = 29
+max_epoch = 120
+data_interval = 5
 def render_all_epoch():
     input_dir  = "./examples/train_result"
     output_dir = input_dir
@@ -16,7 +17,8 @@ def render_all_epoch():
         os.makedirs(output_dir)
     
     idx = 0
-    for idx in range(max_epoch):
+    for idx in range(0,max_epoch,data_interval):
+
         path = os.path.join(input_dir,f"{idx}.npy")
         next_path = os.path.join(input_dir,f"{idx+1}.npy")
         if(idx == max_epoch-1):
